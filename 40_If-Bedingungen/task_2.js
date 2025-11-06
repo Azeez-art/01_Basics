31d1020cf9aa1acd81a627b02aa96834c3519cf7
Azeez <asiseu141@gmail.com>
2025-11-06 12:54:26 +0100

    aufgabe 41 done

--- 40_If-Bedingungen/task_2.js
+++ 40_If-Bedingungen/task_2.js
@@ -1,15 +1,14 @@
 let trafficLight = "green";
 
-if (trafficLight == "red") { 
-    //Blockklammern
-    console.log("Stop");
+if (trafficLight == "red") {
+  //Blockklammern
+  console.log("Stop");
 }
-if(trafficLight == "yellow") {
-    //Blockklammern
-    console.log("Caution");
-    
+if (trafficLight == "yellow") {
+  //Blockklammern
+  console.log("Caution");
 }
-if(trafficLight == "green") {
-    //Blockklammern
-    console.log("You may go");
+if (trafficLight == "green") {
+  //Blockklammern
+  console.log("You may go");
 }
--- 41_If-Aufgaben-Else/index.js
+++ 41_If-Aufgaben-Else/index.js
@@ -1,6 +1,20 @@
-let a = 5
-if(a > 0) {
-    //Blockklammern
-    console.log("The number is positive");
-    
+let a = 9;
+if (a > 0) {
+  console.log("The number is positive");
+} else {
+  console.log("The number is not positive");
+}
+
+//
+
+if (a < 0) {
+  console.log("The number is negative");
+} else {
+  console.log("The number is positiv or 0");
+}
+
+if (a == 0) {
+  console.log("The number is zero");
+} else {
+  console.log("The number is not 0");
 }
