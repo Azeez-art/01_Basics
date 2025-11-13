fe2f8394f124a49af9adf975b7cd13a4b44604ba
Azeez <asiseu141@gmail.com>
2025-11-13 13:05:14 +0100

    45 task 2 done

--- 40_If-Bedingungen/task_2.js
+++ 40_If-Bedingungen/task_2.js
@@ -1,14 +1,11 @@
 let trafficLight = "green";
 
 if (trafficLight == "red") {
-  //Blockklammern
   console.log("Stop");
 }
 if (trafficLight == "yellow") {
-  //Blockklammern
   console.log("Caution");
 }
 if (trafficLight == "green") {
-  //Blockklammern
   console.log("You may go");
 }
---
+++ 45_Deno/task_2.js
@@ -0,0 +1,16 @@
+let a = 0;
+if (a > 0) {
+  console.log("The number is positive");
+} else {
+  console.log("The number is not positive");
+}
+if (a < 0) {
+  console.log("The number is negative");
+} else {
+  console.log("The number is positiv or 0");
+}
+if (a == 0) {
+  console.log("The number is zero");
+} else {
+  console.log("The number is not 0");
+}
