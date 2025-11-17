955a1b2e06be89d1ed9c3790d9c8fe6579e134d0
Azeez <asiseu141@gmail.com>
2025-11-17 14:35:09 +0100

    45 task 3 done

--- 30_Aufgaben-Variabeln/index.js
+++ 30_Aufgaben-Variabeln/index.js
@@ -5,18 +5,20 @@ let apple = "Apple";
 let bananaPricePerKilo = 2.14;
 let applePricePerKilo = 3.43;
 let appleWeight = 0.34;
-
 let bananaWeight = 0.22;
+
 let bananasPerKilo = 1 / bananaWeight;
 let applesPerKilo = 1 / appleWeight;
+
 let pricePerBanana = bananaPricePerKilo * bananaWeight;
 let pricePerApple = applePricePerKilo * appleWeight;
+
 let priceOf8Apples = pricePerApple * 8;
+let priceOf17Bananas = pricePerBanana * 17;
+
 let res1 = priceOf8Apples;
 
-let priceOf17Bananas = pricePerBanana * 17;
 let pricePerTonneApples = applePricePerKilo * 1000;
-
 let pricePerTonneBananas = bananaPricePerKilo * 1000;
 
 console.log(res1);
---
+++ 45_Deno/task_3.js
@@ -0,0 +1,17 @@
+let zahl1 = prompt("zahl 1");
+let zahl2 = prompt("zahl 2");
+let rechenart = prompt("Operation [+,-,*,/]");
+
+if (rechenart === "+") {
+  console.log(zahl1 + zahl2);
+}
+if (rechenart === "-") {
+  console.log(zahl1 - zahl2);
+}
+if (rechenart === "*") {
+  console.log(zahl1 * zahl2);
+}
+if (rechenart === "/") {
+  console.log(zahl1 / zahl2);
+}
+l;
