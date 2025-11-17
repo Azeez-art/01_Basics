21cb571dc464b71cefe0a4519b853b12a51108ee
Azeez <asiseu141@gmail.com>
2025-11-17 14:47:19 +0100

    45 task 4 done (brauche bisschen hilfe bei dieser Aufgabe)

---
+++ 45_Deno/task_4.js
@@ -0,0 +1,12 @@
+let age = 90;
+if (age < 18) {
+  console.log("jung");
+}
+if (age < 50 && age >= 18) {
+  console.log("mittelalt");
+}
+if (age > 50) {
+  console.log("alt");
+}
+
+//nicht sicher wie es funktioniert
