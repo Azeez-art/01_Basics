2682f8277563d2227d797a312ec9dccd564c096a
Azeez <asiseu141@gmail.com>
2025-11-21 14:03:58 +0100

    45 task 6 done

---
+++ 45_Deno/task_6.js
@@ -0,0 +1,16 @@
+let input = prompt("Welches Getränk wollen Sie haben?");
+
+console.log("input", input);
+
+if (input == "Cola") {
+  console.log("Danke für deine Bestellung von Cola");
+}
+if (input == "Wasser") {
+  console.log("Danke für deine Bestellenung von Wasser");
+}
+if (input == "Bier") {
+  console.log("Danke für deine Bestellenung von Bier");
+}
+if (input == "Tee") {
+  console.log("Danke für deine Bestellenung von Tee");
+}
