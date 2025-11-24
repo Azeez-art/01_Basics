062f20a379ae10c6a110e6c806482fc5c485d809
Azeez <asiseu141@gmail.com>
2025-11-24 14:57:11 +0100

    52 task 1 done

--- 52_Logische_Operatoren/task1.js
+++ 52_Logische_Operatoren/task1.js
@@ -5,5 +5,5 @@ let humidity = Number(b);
 if (input < 26 && input > 19 && humidity < 60) {
   console.log("Perfect day for outdoor activities!");
 } else {
-  console.log("Maybe stay indoors today");
+  console.log("Maybe stay indoors for today");
 }
