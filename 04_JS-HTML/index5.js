e5f389ae70f4ab52aed9e1f8fce29be2ddc81d4a
Azeez <asiseu141@gmail.com>
2026-05-28 13:06:56 +0200

    fünftes Viedeo noch nicht fertig!!

---
+++ 04_JS-HTML/index5.js
@@ -0,0 +1,9 @@
+
+let data = [{firstName: "Hans", lastName: "Huber", points:30}];
+
+let person = {firstName: "Hans", lastName: "Huber", points:30};
+
+
+console.log(person.firstName);
+
+function loadPeople() {
