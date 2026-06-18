06fee8d038f083044adaa3d61ae87d161c35e3c4
Azeez <asiseu141@gmail.com>
2026-06-18 13:06:16 +0200

    fünftes Viedeo fertig

--- 04_JS-HTML/index5.js
+++ 04_JS-HTML/index5.js
@@ -1,9 +1,19 @@
+let data = [
+  { firstName: "Hans", lastName: "Huber", points: 30 },
+  { firstName: "Paul", lastName: "Müller", points: 34 },
+  { firstName: "Franz", lastName: "Maier", points: 35 },
+];
 
-let data = [{firstName: "Hans", lastName: "Huber", points:30}];
-
-let person = {firstName: "Hans", lastName: "Huber", points:30};
-
-
-console.log(person.firstName);
+for (let index = 0; index < data.length; index++) {
+  const element = data[index];
+  console.log(element.firstName);
+}
 
 function loadPeople() {
+  let html = "";
+  (data.forEach((element) => {
+    html += "<div>" + element.firstName + " " + element.lastName + "</div>";
+  }),
+    (document.getElementById("content").innerHTML = html));
+}
+loadPeople();
